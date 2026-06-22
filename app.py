47a01e14aa064b6741a10fde8bd622df631ceed6
shayanalam2 <shayanalam2212@gmail.com>
2026-06-22 16:57:50 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("my calculator app")
+num1=st.number_input("enter first number")
+num2=st.number_input("enter your second number")
+
+operation= st.selectbox("choose operation",["+","-","*","/"])
+if st.button("calculate"):
+ if operation =="+":
+    st.write(num1+num2)
+elif operation == "-":
+    st.write(num1-num2)
+elif operation == "*":
+    st.write("*")
+elif operation=="/":
+    st.write(num1/num2)
+else:
+    st.write(operation)
