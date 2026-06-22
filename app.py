b2d9c62d2f0d4831a2570458649c8e348e1bb823
shayanalam2 <shayanalam2212@gmail.com>
2026-06-22 17:10:19 +0500

    Fix typo in selectbox function call

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title("shayan")
 num1=st.number_input("enter first number")
 num2=st.number_input("enter your second number")
 
-operation= st.selectbox("choose operation",["+","-","*","/"])
+operation= st.selectboxint("choose operation",["+","-","*","/"])
 if st.button("calculate"):
  if operation =="+":
     st.write(num1+num2)
